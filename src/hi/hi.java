292cfc12749555b6dc29bea31b8c6b7d4938bdfc
switch <hob4419@gmail.com>
2021-12-21 22:53:13 +0900

    abc

--- src/hi/hi.java
+++ src/hi/hi.java
@@ -3,7 +3,10 @@ package hi;
 public class hi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("abc");
+		System.out.println("abc");
+		System.out.println("abc");
+		System.out.println("¤½¤½¤½");
 
 	}
 
